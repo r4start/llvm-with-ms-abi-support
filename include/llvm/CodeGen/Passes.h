95735826a1e605438ff2a57547b805228e6719c1
r4start <r4start@local.host>
2012-10-11 19:57:25 +0400

    Add AT&T asm syntax generation for SEH inline asm commands.

--- include/llvm/CodeGen/Passes.h
+++ include/llvm/CodeGen/Passes.h
@@ -482,7 +482,7 @@ namespace llvm {
   FunctionPass *createSjLjEHPreparePass(const TargetLowering *tli);
 
   /// r4start
-  FunctionPass *createSEHPreparePass();
+  FunctionPass *createSEHPreparePass(const TargetMachine *tm);
 
   /// LocalStackSlotAllocation - This pass assigns local frame indices to stack
   /// slots relative to one another and allocates base registers to access them
